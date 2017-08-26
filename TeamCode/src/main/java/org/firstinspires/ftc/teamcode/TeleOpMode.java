3f10d5f210846ecb76ab2df6ca34e4e8c717489a
Jonathan Okasinksi <nitrocalcite@users.noreply.github.com>
2017-08-25 20:47:51 -0400

    Added teleop annotation to TeleOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -1,13 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.teamcode.extension.DbzOpMode;
 
 /**
  * Created by Matthew on 8/25/2017.
  */
 
+@TeleOp(name = "MainTeleOp", group = "TeleOp")
 public class TeleOpMode extends DbzOpMode {
-
 
     @Override
     protected void dbzInit() {
